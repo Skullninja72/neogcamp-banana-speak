6937f3d34e675b82bd31c9ee6e6b2b26697df655
Skullninja72 <ameyjoshi2001rj@gmail.com>
2020-12-05 19:33:13 +0530

    done

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var outputDiv = document.querySelector("#output");
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+
+
+
 function getTranslationURL(text) {
      return serverURL + "?" + "text=" + text
 }
